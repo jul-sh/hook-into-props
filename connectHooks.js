ea56ee019a6d63a8ef249d3c6b4bf1c3c386bb14
Juliette Prétot <hi@juliette.sh>
2019-02-09 20:39:21 +0100

    remove comments from connectHook.js

--- connectHooks.js
+++ connectHooks.js
@@ -1,6 +1,3 @@
-// I'm not sure a HoC for hooks is a good idea, but it seemed fun to build.
-// Also, it may come in handy when adding new code to old components.
-
 import React from "react"
 
 const connectHooks = hooks => Component => props => {
@@ -19,37 +16,5 @@ const connectHooks = hooks => Component => props => {
 
   return <HookProvider hooks={hooks} Component={Component} />
 }
-
-/* 
-Use like so:
-
-export default connectHooks([
-  {
-    hook: useFetch,
-    hookArgs: ["https://swapi.co/api/people/1"],
-    toProps: ([isLoading, data]) => ({ isLoading, data })
-  }
-])(
-  class ClassComponent extends React.Component {
-   // ...
-   render () {
-     this.props.isLoading ? "loading" : this.props.data
-   }
-  }
-)
-
-Compose hooks into higherOrders like so:
-
-const withWindowSizeAndScrollPosition = connectHooks([
-  {
-    hook: useWindowScrollPosition,
-    toProps: scrollPosition => ({ scrollPosition })
-  },
-  {
-    hook: useWindowSize,
-    toProps: windowSize => ({ windowSize })
-  }
-])
-*/
 
 export default connectHooks
