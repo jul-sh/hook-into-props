f060fd5bad40a3fbbe11893b8fa7ce503b7b2485
Juliette Pretot <hi@juliette.sh>
2019-03-15 14:08:08 +0100

    rename HooksProvider to HookResultProvider for clarity

--- hookToProps.js
+++ hookToProps.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const hookToProps = useHooks => Component => {
-  const HooksProvider = props => <Component {...props} {...useHooks(props)} />
+  const HookResultProvider = props => <Component {...props} {...useHooks(props)} />
 
-  return HooksProvider
+  return HookResultProvider
 }
 
 export default hookToProps
