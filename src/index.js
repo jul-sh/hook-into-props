6fedf7c0de99550c3eef24fb542a6e66089c031f
Juliette Pretot <hi@juliette.sh>
2019-03-17 00:28:59 +0100

    change name to hookIntoProps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const hookToProps = useHooks => Component => {
+const hookIntoProps = useHooks => Component => {
   const HooksProvider = props => <Component {...props} {...useHooks(props)} />
 
   return HooksProvider
 }
 
-export default hookToProps
+export default hookIntoProps
