51d0fdeef513148e32aa5e9fc6d7b8a8809b4580
Juliette Pretot <hi@juliette.sh>
2019-03-20 23:26:14 +0100

    prefer React.createElement syntax over jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 
 const hookIntoProps = useHooks => Component => {
-  const HooksProvider = React.forwardRef((props, ref) => (
-    <Component ref={ref} {...props} {...useHooks(props)} />
-  ))
+  const HooksProvider = React.forwardRef((props, ref) =>
+    React.createElement(Component, { ...props, ref, ...useHooks(props) })
+  )
 
   HooksProvider.displayName = `HookIntoProps(${Component.displayName ||
     Component.name ||
