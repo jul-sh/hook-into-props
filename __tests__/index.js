548bda24407ad5e5480bea1502f283ccde09575e
Juliette Pretot <hi@juliette.sh>
2019-03-21 19:24:47 +0100

    add additional test

--- __tests__/index.js
+++ __tests__/index.js
@@ -5,69 +5,93 @@ const React = require('react')
 const { render, waitForElement, fireEvent } = require('react-testing-library')
 const hookIntoProps = require('../dist/index.js')
 
-class ExampleComponent extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange() {
-    this.props.setCount(count => count + 1)
-  }
-  render() {
-    return React.createElement(
-      'div',
-      {},
-      React.createElement('h1', {}, `Hello ${this.props.name || 'stranger'}!`),
-      React.createElement('span', {}, `Count: ${this.props.count}`),
-      React.createElement(
-        'button',
-        { onClick: this.handleChange },
-        'Click to increase count'
-      )
-    )
-  }
-}
-
-ExampleComponent.foo = 'bar'
-
-const useHooks = ({ initialCount = 0 }) => {
-  const [count, setCount] = React.useState(initialCount)
-
-  return { count, setCount }
-}
-
-const WrappedExampleComponent = hookIntoProps(useHooks)(ExampleComponent)
-
 describe('The `hookIntoProps` helper', () => {
   it('passes props through to the wrapped Component', async () => {
+    const MockComponent = props => props.name
+    const useHooks = () => {}
+    const WrappedMockComponent = hookIntoProps(useHooks)(MockComponent)
+
     const { getByText } = render(
-      React.createElement(WrappedExampleComponent, { name: 'Jules' })
+      React.createElement(WrappedMockComponent, { name: 'Jules' })
     )
 
-    await waitForElement(() => getByText(/Hello Jules!/i))
+    await waitForElement(() => getByText(/Jules/i))
   })
 
   it('calls hooks based on props', async () => {
+    const MockComponent = props => `Count: ${props.count}`
+    const useHooks = props => {
+      const [count] = React.useState(props.initialCount)
+
+      return { count }
+    }
+    const WrappedMockComponent = hookIntoProps(useHooks)(MockComponent)
+
     const { getByText } = render(
-      React.createElement(WrappedExampleComponent, { initialCount: 7 })
+      React.createElement(WrappedMockComponent, { initialCount: 5 })
     )
 
-    await waitForElement(() => getByText(/Count: 7/i))
+    await waitForElement(() => getByText(/Count: 5/i))
   })
 
   it('updates if required', async () => {
-    const { getByText } = render(
-      React.createElement(WrappedExampleComponent, { initialCount: 1 })
-    )
+    class MockComponent extends React.Component {
+      constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+      }
+      handleChange() {
+        this.props.setCount(count => count + 1)
+      }
+      render() {
+        return React.createElement(
+          'div',
+          {},
+          React.createElement('span', {}, `Count: ${this.props.count}`),
+          React.createElement(
+            'button',
+            { onClick: this.handleChange },
+            'Click to increase count'
+          )
+        )
+      }
+    }
+    const useHooks = () => {
+      const [count, setCount] = React.useState(0)
 
+      return { count, setCount }
+    }
+    const WrappedMockComponent = hookIntoProps(useHooks)(MockComponent)
+
+    const { getByText } = render(React.createElement(WrappedMockComponent))
     fireEvent.click(getByText('Click to increase count'))
 
-    await waitForElement(() => getByText(/Count: 2/i))
+    await waitForElement(() => getByText(/Count: 1/i))
+  })
+
+  it('passes no additional props', async () => {
+    const MockComponent = props =>
+      `List of props: "${Object.keys(props).toString()}"`
+    const useHooks = () => {
+      const [count, setCount] = React.useState(0)
+
+      return { count, setCount }
+    }
+    const WrappedMockComponent = hookIntoProps(useHooks)(MockComponent)
+
+    const { getByText } = render(React.createElement(WrappedMockComponent))
+
+    await waitForElement(() => getByText(/List of props: "count,setCount"/i))
   })
 
   it('hoists statics of the wrapped Component', () => {
-    const actual = WrappedExampleComponent.foo
-    const expected = ExampleComponent.foo
+    const MockComponent = props => null
+    MockComponent.foo = 'bar'
+    const useHooks = () => {}
+    const WrappedMockComponent = hookIntoProps(useHooks)(MockComponent)
+
+    const actual = WrappedMockComponent.foo
+    const expected = MockComponent.foo
     expect(actual).toEqual(expected)
   })
 })
