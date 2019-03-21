4c2122fb2fa716772e77546c3dd87b3a84e0ec9e
Juliette Pretot <hi@juliette.sh>
2019-03-21 18:34:45 +0100

    remove ref forwarding, as it can cause issues when using hookToProps in HoC

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 
 const hookIntoProps = useHooks => Component => {
-  const HooksProvider = React.forwardRef((props, ref) =>
+  const HooksProvider = (props, ref) =>
     React.createElement(Component, { ...props, ref, ...useHooks(props) })
-  )
 
   HooksProvider.displayName = `HookIntoProps(${Component.displayName ||
     Component.name ||
