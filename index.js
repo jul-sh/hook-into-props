083faa2107442552955f5b224bb1756add1c8222
Juliette Pretot <m@juliette.sh>
2019-05-21 17:45:31 -0400

    change source code to function declarations

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 
-const hookIntoProps = useHooks => Component => {
-  const HooksProvider = props =>
-    React.createElement(Component, { ...props, ...useHooks(props) })
+function hookIntoProps(useHooks) {
+  return function(Component) {
+    function HooksProvider(props) {
+      return React.createElement(Component, { ...props, ...useHooks(props) })
+    }
 
-  HooksProvider.displayName = `HookIntoProps(${Component.displayName ||
-    Component.name ||
-    'Component'})`
+    HooksProvider.displayName = `HookIntoProps(${Component.displayName ||
+      Component.name ||
+      'Component'})`
 
-  return hoistNonReactStatics(HooksProvider, Component)
+    return hoistNonReactStatics(HooksProvider, Component)
+  }
 }
 
 export default hookIntoProps
